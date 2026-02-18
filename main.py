8fdbc22e6773ea3cbfc4402a336a15ff397f09f0
Muneeb Mennad <mennadmuneeb@gmail.com>
2026-02-18 16:25:23 -0330

    add quit confirm

--- main.py
+++ main.py
@@ -17,19 +17,35 @@ def run_program():
 
     # Program goes to login if user credentials are already setup
     if not authentication.has_master_credentials():
-        menus.setup_menu()
-    loginSuccessful = menus.login_menu()
+        menus.show_setup_menu()
+        
+    loginSuccessful = menus.show_login_menu()
+    
     if loginSuccessful:
         print("\nWelcome,\033[1m", system.whoami(), "\033[0m")
         menus.section_cutter()
         input("Press enter to continue to main menu... ")
-        system.clear_screen()
-        menus.main_menu()
+        
+        while True:
+            system.clear_screen()
+            choice = menus.show_main_menu()
+            
+            if choice == 1:
+                system.clear_screen()
+                menus.show_passgen_menu()
+            elif choice == 2:
+                system.clear_screen()
+                menus.show_passcheck_menu()
+            elif choice == 3:
+                confirmed = int(input("Enter 1 to confirm: "))
+                if confirmed:
+                    system.exit()
+                    break
+                else:
+                    continue
     else:
-        print()
-        print("Login Unsuccessful")
-        print()
-        input("Press enter to exit... ")
+        print("\nLogin Unsuccessful")
+        input("\nPress enter to exit... ")
         system.exit()
 
 
